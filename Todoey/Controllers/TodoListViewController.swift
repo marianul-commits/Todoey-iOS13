45467b27bc0a0fb567082b43a19e75a4a9c98aa7
marianul-commits <80788036+marianul-commits@users.noreply.github.com>
2023-06-24 16:57:02 +0300

    Added CoreData

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
     
@@ -15,37 +16,27 @@ class TodoListViewController: UITableViewController {
     
     let defaults = UserDefaults.standard
     
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem.title = "Destroy the gov"
-        itemArray.append(newItem)
-        
-        let newItem3 = Item()
-        newItem.title = "get rich die trying"
-        itemArray.append(newItem)
-        
-        if let items = defaults.array(forKey: "ToDoListArray") as? [Item]{
-            itemArray = items
-            
-        }
+        loadItems()
     }
         
         //MARK: - Tableview Datasource Methods
         
         override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            guard (itemArray != nil) else{
+                return 0
+            }
             return itemArray.count
         }
         
         override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             
             let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-            
+                        
             let item = itemArray[indexPath.row]
             
             cell.textLabel?.text = item.title
@@ -66,8 +57,9 @@ class TodoListViewController: UITableViewController {
             
             itemArray[indexPath.row].done = !itemArray[indexPath.row].done
             
-            tableView.reloadData()
+            saveItems()
             
+            tableView.deselectRow(at: indexPath, animated: true)
         }
         
         //MARK: -- Add new items
@@ -75,24 +67,23 @@ class TodoListViewController: UITableViewController {
         @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
             
             var textField = UITextField()
-            
-            let newItem = Item()
-            newItem.title = textField.text ?? ""
-            
+                        
             let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
             
             let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+                
+                let newItem = Item(context: self.context)
+                newItem.title = textField.text!
+                newItem.done = false
+                
                 if textField.text != ""{
                     self.itemArray.append(newItem)
-                    self.defaults.set(self.itemArray, forKey: "ToDoListArray")
-                    self.tableView.reloadData()
-                    print("added")
+                    self.saveItems()
                 } else {
                     let error = UIAlertController(title: "Error", message: "Cannot add empty task", preferredStyle: .alert)
                     let errorAction = UIAlertAction(title: "OK", style: .default)
                     error.addAction(errorAction)
                     self.present(error, animated: true, completion: nil)
-                    print("not added")
                 }
                 
             }
@@ -108,6 +99,27 @@ class TodoListViewController: UITableViewController {
             
         }
         
-        
+    //MARK: - Model Manipulation Methods
+    
+    func saveItems() {
+        do{
+            try context.save()
+        } catch {
+           print("error saving context \(error)")
+        }
+        tableView.reloadData()
     }
+    
+    func loadItems() {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        do{
+           itemArray = try context.fetch(request)
+        } catch {
+            print("error fetching \(error)")
+        }
+    }
+    
+    
+    
+}
     
